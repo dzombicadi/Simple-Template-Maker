881cf9c7f8f91b5d85d1faed9ca3ab6cc0313956
dzombicadi <dzombaxd@gmail.com>
2023-05-02 21:10:57 +0200

    C++ and C# Languages Supported

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 c_ext = "c"
 cpp_ext = "cpp"
+cs_ext = "cs"
 
 c_write = '''#include <stdio.h>
 
@@ -12,11 +13,32 @@ int main() {
 
 }
 '''
+cpp_write = '''#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}'''
+cs_write = '''using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}'''
+
+
 
 def import_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--extension', type=str, required=True, help="Available extensions: C, C++", metavar="[c, cpp]")
-    parser.add_argument('-o', '--output', type=str, required=False, help="Name of the file you want.", metavar="[template.c/template.cpp]")
+    parser.add_argument('-e', '--extension', type=str, required=True, help="Available extensions: C, C++", metavar="[c, cpp, cs]")
+    parser.add_argument('-o', '--output', type=str, required=False, help="Name of the file you want.", metavar="[template.c/template.cpp/template.cs]")
     arguments = parser.parse_args()
 
     print(arguments)
@@ -27,28 +49,38 @@ def import_args():
 def which_ext(extension, output):
     tmp_name = "template"
 
-    if(output == None):
-        print("File name not given, default is: template")
-    else:
-        tmp_name = output
+    if(output != None):
+        tmp_name = output 
 
     if(extension == c_ext):
-        make_c_file(tmp_name)
+        make_file(tmp_name, c_ext)
+    elif(extension == cpp_ext):
+        make_file(tmp_name, cpp_ext)
+    elif(extension == cs_ext):
+        make_file(tmp_name, cs_ext)
     else:
-        print("a")
+        print("Available extensions are: 'c', 'cpp', 'cs'")
 
         
         
-def make_c_file(tmpn):
-    file = open("{}.c".format(tmpn), "a+")
-    file.writelines(c_write)
+def make_file(tmp_name, ext):
+
+    file = open("{}.{}".format(tmp_name, ext), "a+")
+
+    if(ext == c_ext):
+        file.writelines(c_write)
+    elif(ext == cpp_ext):
+        file.writelines(cpp_write)
+    elif(ext == cs_ext):
+        file.writelines(cs_write)
+
     file.close()
+
 
 
 
 if __name__ == '__main__':
     import_args()
-    tmp_name = "template"
 
 
 
